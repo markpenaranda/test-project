f1ede8850882e69fb9b5389525dfbb328f0398d3
MJP <markangelpenaranda@gmail.com>
2017-02-14 02:42:02 +0800

    Fixed UTC issues in countdown timer

--- js/app/chatroom.js
+++ js/app/chatroom.js
@@ -418,6 +418,7 @@ var candidateScreenManagement = (function($) {
         function myTimer() {
             var now = moment();
             var nowUtc = moment.utc(now);
+            console.log(nowUtc);
              var then = moment(openday.event_date + " " + openday.start_time);
 
              var duration = nowUtc - then;
