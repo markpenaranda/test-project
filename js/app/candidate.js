77aec7cfaf36186b43d6d3e07315a7d7e5bee70d
MJP <markangelpenaranda@gmail.com>
2017-02-13 12:42:47 +0800

    JS Videocall

--- js/app/candidate.js
+++ js/app/candidate.js
@@ -290,3 +290,36 @@ var candidateScreenManagement = (function($) {
 
 })($);
 $(candidateScreenManagement.init);
+
+
+//
+
+var peer = new Peer('openday-' + $("#userId").val(),  {host: 'openday.jobsglobal.com', secure:true, key: 'peerjs'});
+
+window.peer = peer;
+
+peer.on('call', function(call) {
+  // var navGetUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+  navigator.getUserMedia({video: true, audio: true}, function(stream) {
+     var localVideo = document.getElementById('localVideo');
+    localVideo.srcObject = stream;
+    call.answer(stream); // Answer the call with an A/V stream.
+    call.on('stream', function(remoteStream) {
+        var remoteVideo = document.getElementById('remoteVideo');
+      remoteVideo.srcObject = remoteStream;
+       $("#remoteVideo").css("height", "100%");
+       $("#remoteVideo").css("margin-left", "auto");
+       $("#remoteVideo").css("margin-right", "auto");
+       $("#remoteVideo").css("top", "50%");
+       $("#remoteVideo").css("left", "50%");
+       $("#remoteVideo").css("transform", "translate(-50%, -50%)");
+
+         var display = $('#lapseTime');
+
+
+       lapseTimer(0, display);
+    });
+  }, function(err) {
+    console.log('Failed to get local stream' ,err);
+  });
+});
